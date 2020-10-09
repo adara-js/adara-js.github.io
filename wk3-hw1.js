451a8696d5a22d88c88d65ad635ee85987f07072
Adara <adara@sfawakenedmind.org>
2020-10-08 20:47:19 -0700

    HW for wk 3

---
+++ wk3-hw1.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+
+console.log(`Hello ${firstName} ${lastName} you are ${age} years old.`);
+
