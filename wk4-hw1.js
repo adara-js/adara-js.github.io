2cc770cc5a393ffd37efe2c23c7416a744967d3a
Adara <adara@sfawakenedmind.org>
2020-10-11 17:21:51 -0700

    Refactored code

--- wk4-hw1.js
+++ wk4-hw1.js
@@ -1,31 +1,26 @@
 // Is Even
 // Takes a number and returns true if even and false if odd
 function isEven(number) {
-	 if(number%2 === 0) {
-	 	return true;
-	 } 
-
-	 return false;
+	return number%2 === 0;
 }
 
 // Factorial
 // Takes a number and returns the factorial
 function factorial(number) {
-	if (number === 0) {
-    return 1;
+  if (number === 0) {
+     return 1;
 	}
 	
-  let arr = [];
+	let factorial = 1;
 	for (i = number; i>0; i--) {
-		arr.push(i);
+		factorial *= i;
 	}  
   
-  return arr.reduce((a,b) => a*b);
+  	return factorial;
 }
 
 // kebabToSnake
 // Takes a string in kebab case and converts it to snake case
-
 function kebabToSnake(string) {
 	return string.replace(/-/g,'_');
 }
